168c7e1ce6f4caa02bfeea23d44fca014335b3ad
Angel Guerrero <ajguerreror@gmail.com>
2021-08-09 18:15:25 -0400

    Crear route y controller de createUser (POST)

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-
+const User = require('../models/user');
 
 
 const getUsers = (req, res) => {
@@ -8,4 +8,21 @@ const getUsers = (req, res) => {
   })
 }
 
-module.exports = { getUsers };
+const createUser = async (req, res) => {
+
+  const { name, password, email } = req.body;
+  const user = new User({
+    name,
+    email,
+    password
+  });
+
+  await user.save();
+
+  res.json({
+    ok: true,
+    user
+  })
+}
+
+module.exports = { getUsers, createUser };
