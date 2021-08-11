542be6c00e2a0a853fbcc2a99177b330b0f669a5
Angel Guerrero <ajguerreror@gmail.com>
2021-08-11 17:57:36 -0400

    Crea ruta y controller de actualizar user

--- controllers/users.js
+++ controllers/users.js
@@ -48,4 +48,45 @@ const createUser = async (req, res = response) => {
   }
 }
 
-module.exports = { getUsers, createUser };
+const updateUser = async (req, res = response) => {
+  const uid = req.params.id;
+  try {
+
+    const userDB = await User.findById( uid );
+    if ( !userDB ) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'User not found'
+      });
+    }
+
+    const fields = req.body;
+    delete fields.password;
+    delete fields.google;
+
+    if (userDB.email !== fields.email) {
+      const emailExist = await User.findOne({ email: fields.email });
+      if (emailExist) {
+        return res.status(400).json({
+          ok: false,
+          msg: 'Email exists'
+        });
+      }
+    }
+
+    const updatedUser = await User.findByIdAndUpdate( uid, fields, { new: true } );
+
+    res.json({
+      ok: true,
+      user: updatedUser
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error inesperado... revisar logs'
+    });
+  }
+}
+
+module.exports = { getUsers, createUser, updateUser };
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const { check } = require('express-validator');
 
 const { validate } = require('../middlewares/validators')
-const { getUsers, createUser } = require('../controllers/users')
+const { getUsers, createUser, updateUser } = require('../controllers/users')
 
 const router = Router();
 
@@ -15,7 +15,18 @@ router.post('/',
     check('email', 'Invalid email format').isEmail(),
     validate
   ],
-  createUser);
+  createUser
+);
+router.put('/:id',
+  [
+    check('name', 'Name is required').notEmpty(),
+    check('email', 'Email is required').notEmpty(),
+    check('email', 'Invalid email format').isEmail(),
+    check('role', 'Role is required').notEmpty(),
+    validate
+  ],
+  updateUser
+);
 
 
 module.exports = router;
