41933d434c7aa491eeb6b170b95e3fd6748e1b39
Angel Guerrero <ajguerreror@gmail.com>
2021-08-11 16:15:41 -0400

    Devuelve el _id como uid

--- models/user.js
+++ models/user.js
@@ -27,6 +27,12 @@ const UserSchema = Schema({
     type: Boolean,
     default: false
   }
-})
+});
+
+UserSchema.method('toJSON', function(){
+  const { __v, _id, ...object } = this.toObject();
+  object.uid = _id;
+  return object;
+});
 
 module.exports = model('User', UserSchema);
