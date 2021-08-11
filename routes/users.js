b03f52952c58559505a90408a5afcc2eedd31961
Angel Guerrero <ajguerreror@gmail.com>
2021-08-11 16:55:14 -0400

    Utiliza middleware para validar datos recibidos del obj req

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,21 @@
 const { Router } = require("express");
+const { check } = require('express-validator');
+
+const { validate } = require('../middlewares/validators')
 const { getUsers, createUser } = require('../controllers/users')
 
 const router = Router();
 
 router.get('/', getUsers);
-router.post('/', createUser);
+router.post('/',
+  [
+    check('name', 'Name is required').notEmpty(),
+    check('password', 'Password is required').notEmpty(),
+    check('email', 'Email is required').notEmpty(),
+    check('email', 'Invalid email format').isEmail(),
+    validate
+  ],
+  createUser);
 
 
 module.exports = router;
