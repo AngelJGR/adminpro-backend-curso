8c3accdae94f56370580796529a403546923e754
Angel Guerrero <ajguerreror@gmail.com>
2021-08-13 16:07:34 -0400

    Crea helper para validar token

---
+++ middlewares/validate-jwt.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const validateJwt = (req, res, next) => {
+  const token = req.header('x-token');
+
+  if (!token) {
+    res.status(401).json({
+      ok: false,
+      msg:'No token provided'
+    });
+  }
+
+  try {
+
+    const { uid } = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(uid);
+    
+  } catch (error) {
+    res.status(401).json({
+      ok: false,
+      msg: 'Invalid token'
+    });
+  }
+
+  next();
+}
+
+module.exports = {
+  validateJwt
+}
