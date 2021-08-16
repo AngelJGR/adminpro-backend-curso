0bee7c5b86c03e9d35a79fb80eec34c34246392b
Angel Guerrero <ajguerreror@gmail.com>
2021-08-16 18:12:27 -0400

    Crea hospital con validaciones en req.body

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const Hospital = require('../models/hospital');
 
 const getHospitals = (req, res = response) => {
   res.json({
@@ -7,11 +8,26 @@ const getHospitals = (req, res = response) => {
   })
 }
 
-const createHospital = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'Create'
-  })
+const createHospital = async (req, res = response) => {
+
+  const hospital = new Hospital({
+    user: req.uid,
+    ...req.body
+  });
+
+  try {
+    const hospitalDB = await hospital.save();
+    res.json({
+      ok: true,
+      hospital: hospitalDB
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error inesperado... revisar logs'
+    });
+  }
 }
 
 const updateHospital = (req, res = response) => {
--- routes/hospitals.js
+++ routes/hospitals.js
@@ -3,12 +3,17 @@ const { check } = require('express-validator');
 
 const { getHospitals, createHospital, updateHospital, deleteHospital } = require('../controllers/hospitals');
 const { validateJwt } = require("../middlewares/validate-jwt");
+const { validate } = require('../middlewares/validators')
 
 const router = Router();
 
 router.get('/', getHospitals);
 router.post('/',
-  //[],
+  [
+    validateJwt,
+    check('name', 'Hospital name is required').notEmpty(),
+    validate
+  ],
   createHospital
 );
 router.put('/:id',
