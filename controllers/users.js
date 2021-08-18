bdbc34963c781c621cd474ece449250de349ab16
Angel Guerrero <ajguerreror@gmail.com>
2021-08-18 11:44:16 -0400

    Pagina gets de users

--- controllers/users.js
+++ controllers/users.js
@@ -6,11 +6,24 @@ const { generateJWT } = require('../helpers/jwt');
 
 
 const getUsers = async (req, res) => {
-  const users = await User.find({}, 'name email role google');
+  const from = Number(req.query.from) || 0;
+  // const users = await User
+  //   .find({}, 'name email role google')
+  //   .skip(from)
+  //   .limit(5);
+
+  const [users, total] = await Promise.all([
+    User
+      .find({}, 'name email role google')
+      .skip(from)
+      .limit(5),
+    User.countDocuments()
+  ]);
+  
   res.json({
     ok: true,
     users,
-    uid: req.uid
+    total
   })
 }
 
