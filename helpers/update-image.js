2124cef1306bd6259ba66f6debf98021794e1ada
Angel Guerrero <ajguerreror@gmail.com>
2021-08-19 17:55:19 -0400

    Añade guardado de img para hospitals y users

--- helpers/update-image.js
+++ helpers/update-image.js
@@ -4,8 +4,14 @@ const User = require('../models/user');
 const Doctor = require('../models/doctor');
 const Hospital = require('../models/hospital');
 
+const deleteImage = (path) => {
+  if ( fs.existsSync(path) ) {
+    fs.unlinkSync(path);
+  }
+}
+
 const updateImage = async (collection, id, filename) => {
-  
+  let oldPath = '';
   switch (collection) {
     case 'doctors':
       const doctor = await Doctor.findById(id);
@@ -13,24 +19,44 @@ const updateImage = async (collection, id, filename) => {
         console.log('Doctor not found');
         return false;
       }
-      const oldPath = `./uploads/doctors/${doctor.img}`;
-      // Delete old img
-      if ( fs.existsSync(oldPath) ) {
-        fs.unlinkSync(oldPath);
-      }
+      oldPath = `./uploads/doctors/${doctor.img}`;
+      deleteImage(oldPath);
+      
       doctor.img = filename;
       await doctor.save();
+      return true;
+    break;
 
-    break;
     case 'hospitals':
+      const hospital = await Hospital.findById(id);
+      if (!hospital) {
+        console.log('Hospital not found');
+        return false;
+      }
+      oldPath = `./uploads/hospitals/${hospital.img}`;
+      deleteImage(oldPath);
       
+      hospital.img = filename;
+      await hospital.save();
+      return true;
     break;
+
     case 'users':
+      const user = await User.findById(id);
+      if (!user) {
+        console.log('User not found');
+        return false;
+      }
+      oldPath = `./uploads/users/${user.img}`;
+      deleteImage(oldPath);
       
+      user.img = filename;
+      await user.save();
+      return true;
     break;
   
-    default:
-    break;
+    // default:
+    // break;
   }
 
 }
